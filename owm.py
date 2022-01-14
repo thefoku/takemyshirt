9645ae3024432fe8cd1ba44f89b1653b120c1b08
thefoku <97766301+thefoku@users.noreply.github.com>
2022-01-15 01:26:18 +0300

    Add files via upload

---
+++ owm.py
@@ -0,0 +1,10 @@
+﻿import telebot
+
+bot = telebot.TeleBot("5041565448:AAFOIB1jmxHtAfZ94FVX_6CM5vLjCaBgLFI")
+
+@bot.message_handler(content_types=['text'])
+def send_echo(message):
+	#bot.reply_to(message, message.text)
+	bot.send_message (message.chat.id, message.text)
+
+bot.polling( none_stop = True)
